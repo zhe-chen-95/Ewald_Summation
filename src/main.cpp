308f63ea137c2a76f9eaa6e4174a71c966c03a57
guanchun <liguanchun@outlook.com>
2019-05-21 10:39:27 -0400

    Modify main.cpp \& add the report & numerical result

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,26 @@ using namespace std;
 // nvcc -std=c++11 -O3   main.cpp ewald.cu global.cpp -lcufft -o ewald
 int main(int argc, char* argv[]){
   int num_threads = 1;
-  cout << "Please input num of threads(default=1): " << endl;
-	cin >> num_threads;
+  // cout << "Please input num of threads(default=1): " << endl;
+	// cin >> num_threads;
+  sscanf(argv[1], "%d", &num_threads); 
+  int N, num_p, P, rp;
+  double L = 2.0, xi_in = 12.0, eta_in = 1.15;  
+  sscanf(argv[2], "%d", &N);
+  sscanf(argv[3], "%d", &num_p);
+  // sscanf(argv[3], "%f", &xi_in);
+  // sscanf(argv[4], "%f", &eta_in);
+  sscanf(argv[4], "%d", &P);
+  sscanf(argv[5], "%d", &rp);
+  // sscanf(argv[7], "%f", &L);  
+  printf("Size N = %d with %d particles \n", N, num_p);
+  printf("Gaussian P = %d, Real layer rp = %d \n", P, rp);
+  printf("Length L = %f, xi = %f, eta = %f", L, xi_in, eta_in);
+
   long tt = clock();
   cout << "\n---------------initialize---------------" << endl;
-  initialize();
+  // initialize();
+  initialize_readinput(N, num_p, P, eta_in, rp, L, xi_in);
   cout << "\n---------------Real Space---------------" << endl;
   realspace();
   cout << "\n---------------Real Space OMP---------------" << endl;
